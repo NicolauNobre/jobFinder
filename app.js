4bbe403b1a9460f22957bc060d7c42bd326d7239
Nicolas Nobre Brasil <nicolaunobreb@gmail.com>
2023-06-27 16:26:10 -0300

    feat: rotas finais + view mobile

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express    = require('express')
+const exphbs     = require('express-handlebars')
 const app        = express()
+const path       = require('path')
 const db         = require('./db/connection')
 const bodyParser = require('body-parser')
+const Job        = require('./models/Job')
+const Sequelize  = require('sequelize')
+const Op         = Sequelize.Op
 
 const PORT = 3000
 
@@ -13,6 +18,15 @@ app.listen(PORT, () => {
 // body parser
 app.use(bodyParser.urlencoded({ extended: false }))
 
+
+// handle bars 
+app.set('views', path.join(__dirname, 'views'))
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+// static folder 
+app.use(express.static(path.join(__dirname, 'public')))
+
 // db connection
 db.authenticate()
     .then(() => {console.log('Database connected...')}
@@ -22,7 +36,34 @@ db.authenticate()
 
 // routes
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+
+    let search = req.query.job
+    let query  = '%'+search+'%'
+
+    if(!search) {
+        Job.findAll({order: [
+            ['createdAt', 'DESC']
+        ]})
+        .then(jobs => {
+            res.render('index', {
+                jobs
+            })
+        })
+        .catch(err => console.log(err))
+    } else {
+        Job.findAll({
+            where: {title: {[Op.like]: query}},
+            order: [
+                ['createdAt', 'DESC']
+            ]
+        })
+        .then(jobs => {
+            res.render('index', {
+                jobs, search
+            })
+        })
+        .catch(err => console.log(err))
+    }
 })
 
 // jobs routes
--- routes/jobs.js
+++ routes/jobs.js
@@ -7,6 +7,24 @@ router.get('/test', (req, res) => {
     res.send('Job route testing!')
 })
 
+// form da rota de envio
+router.get('/add', (req, res) => {
+    res.render('add')
+})
+
+// detalhe da vaga -> view/1, view/2, view/3
+router.get('/view/:id', (req, res) => {
+    Job.findOne({
+        where: {id: req.params.id}
+    })
+    .then(job => {
+        res.render('view', {
+            job
+        })
+    })
+    .catch(err => console.log(err))
+})
+
 // add job via post 
 router.post('/add', (req, res) => {
     let { title, salary, company, description, email, new_job } = req.body
